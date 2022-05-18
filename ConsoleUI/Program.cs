0dbd228ffd5a26661b767e59a9083efdf1445fb4
Furkan Baser <furkanbaserceng@outlook.com>
2022-05-18 19:57:33 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System.Collections.Generic;
+
+ICarService _carService = new CarManager(new InMemoryCarDal());
+
+foreach (var car in _carService.GetAll())
+{
+    Console.WriteLine(car.DailyPrice);
+    Console.WriteLine(car.ModelYear);
+    Console.WriteLine(car.Description);
+    Console.WriteLine("-------------");
+
+}
+
+Console.WriteLine("--------------------------------------------------");
+
+
